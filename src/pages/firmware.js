a4967a4cf7d74e83235dba23d5f14531a437442f
Qun He <hawkbee@qq.com>
2023-05-17 15:50:37 +0800

    Remove duplicate item.

--- src/pages/firmware.js
+++ src/pages/firmware.js
@@ -41,17 +41,7 @@ export default function Firmware() {
               ]}
             />
           </div>
-          <div className="col">
-            <DeviceFirmware
-              model="ZT-RG1782PoE"
-              version="v5.0.build20230511-1534"
-              href="/docs/user"
-              notes={[
-                "Impove network stablity.",
-                "Upgrade upstream to lastest version.",
-              ]}
-            />
-          </div>
+          <div className="col"></div>
         </div>
       </main>
     </Layout>
